8372d050346da2c5072ea3b6971fac879a929536
Jochen Manall <jochenmanall@online.de>
2025-09-12 17:59:52 +0200

    Clean up and keep only files needed for documented Pandoc + Reveal.js + Nginx workflow

--- src/your_app_script.py
+++
@@ -1,9 +0,0 @@
-from nicegui import ui
-
-ui.input(label='Text', placeholder='start typing',
-         on_change=lambda e: result.set_text('you typed: ' + e.value),
-         validation={'Input too long': lambda value: len(value) < 20})
-result = ui.label()
-ui.link('And many more...', '/static/slides.html').classes('mt-8')
-
-ui.run()
